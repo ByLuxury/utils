22e582ba51f183dc21326ef88a925460758a8cbf
周帅 <571840649@qq.com>
2018-07-03 10:49:45 +0800

    init commit

---
+++ signs.go
@@ -0,0 +1,11 @@
+package signs
+
+func GetSign(month, day int) string {
+	//var m = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	var signs = []string{"魔羯", "水瓶", "双鱼", "白羊", "金牛", "双子", "巨蟹", "狮子", "处女", "天秤", "天蝎", "射手"}
+	var days = []int{20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22}
+	if day < days[month-1] {
+		return signs[month-1]
+	}
+	return signs[month]
+}
